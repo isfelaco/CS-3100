d1221d32df2e11cd09aebb524b0553b305244e7d
isfelaco <isfelaco@gmail.com>
2022-12-06 21:22:36 -0500

    WORKING

--- Mod6Scheduling/scheduling.py
+++ Mod6Scheduling/scheduling.py
@@ -1,16 +1,42 @@
-def DFS(currentNode, visited, G, nodes, size):
-    visited.append(currentNode)
+def BFS(G, s, t, parent, size, nodes):
+        visited = [False]*(size)
+        queue = []
+        queue.append(s)
+        visited[s] = True
 
-    if currentNode == "T":
-        return
+        while queue:
+            u = queue.pop(0)
+     
+            for ind, val in enumerate(G[u]):
+                if visited[ind] == False and val > 0:
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+                    if ind == t:
+                        return True
+        return False
 
-    index = nodes.index(currentNode)
+def FordFulkerson(source, sink, G, nodes, size): 
+        parent = [-1]*(size)
+        max_flow = 0
 
-    for i in range(size):
-        if nodes[i] not in visited and G[index][i] != 0:
-            G[index][i] -= 1
-            G[i][index] += 1
-            return DFS(nodes[i], visited, G, nodes, size)
+        while BFS(G, source, sink, parent, size, nodes):
+            path_flow = float("Inf")
+            s = sink
+
+            while(s != source):
+                path_flow = min (path_flow, G[parent[s]][s])
+                s = parent[s]
+ 
+            max_flow +=  path_flow
+            v = sink
+            while(v !=  source):
+                u = parent[v]
+                G[u][v] -= path_flow
+                G[v][u] += path_flow
+                v = parent[v]
+ 
+        return max_flow
 
 
 def scheduling(r, c, n):
@@ -19,17 +45,13 @@ def scheduling(r, c, n):
     n = int(n)  # num classes per student
     
     valid = "Yes"
-    # if r == 0 or n == 0:
-    #     valid = "No"
     if r == 0:
-        # print("huh")
         for i in range(0, c):
             input()
         input()
         print(valid)
         return
     else:
-        # print("here", r, c, n)
         nodes = ["S"]
         students = []
         edges = []
@@ -51,9 +73,6 @@ def scheduling(r, c, n):
         G = []	
         for i in range(size):	
             G.append([0]*size)
-        # for row in G:
-        #     print(row)
-        # print()
 
         for student in students:
             index = nodes.index(student)
@@ -69,32 +88,22 @@ def scheduling(r, c, n):
                 if course not in nodes:
                     nodes.append(course)
                 index = nodes.index(course)
-                # G.append([0]*size)
-                # print(index, size-1)
                 G[index][size-1] = int(row[1])
-        visited = []
-        while len(visited) != 1:
-            visited = []
-            DFS(nodes[0], visited, G, nodes, size)
 
-    # valid = "Yes"
-        # for row in G:
-        #     print(row)
-        for student in students:
-            index = nodes.index(student)
-            if G[0][index] != 0:
-                valid = "No"
+        max_flow = FordFulkerson(0, size-1, G, nodes, size)
+        if max_flow != len(students) * n:
+            valid = "No"
+
         if n == 0:
             valid = "Yes"
         print(valid)
+
         input()
         return
 
 def main():
     while True:
         line = input()
-        # print(line)
-
         if line == "0 0 0":
             break
         else:
